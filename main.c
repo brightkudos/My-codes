17d2b2d0440e8d1c71fc6f91810429dec8b1d519
brightkudos <168169427+brightkudos@users.noreply.github.com>
2024-04-26 10:46:31 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main() {
+  int n, i;
+  float sum= 0, average;
+  printf("Enter the number of elements: ");
+  scanf("%d", &n);
+  if(n<=0){
+    printf("Invalid input. Number of elements must be positive.\n");
+    return 1;
+  }
+  float numbers[n];
+
+  printf("Enter %d numbers: \n", n);
+  for (i=0; i<n; i++){
+    printf("Enter number %d: ", i + 1);
+    scanf("%f", &numbers[i]);
+    sum += numbers[i];
+  }
+
+  average = sum/n;
+  printf("Average of entered numbers = %.2f\n", average);
+  return 0;
+
+
+
+};
